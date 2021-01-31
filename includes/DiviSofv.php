b3816fc0a02a2300a645a65b54ad886cb46163f5
Raffael Wyss <raffael.wyss@gmail.com>
2021-02-01 00:49:12 +0100

    Initial-Commit

---
+++ includes/DiviSofv.php
@@ -0,0 +1,46 @@
+<?php
+
+class DIVI_DiviSofv extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'divi-divi_sofv';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'divi_sofv';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * DIVI_DiviSofv constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'divi_sofv', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new DIVI_DiviSofv;
